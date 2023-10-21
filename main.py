2228ab19e2dbd8ae1105afacb052f9523c88af6d
Jonathan Hirsch <jonathan.hirsch2011@gmail.com>
2023-10-21 15:07:15 -0700

    added functionality to assignrole and added unassignrole function to bot

--- main.py
+++ main.py
@@ -34,8 +34,13 @@ async def hello(ctx):
 
 
 @bot.command()
-@commands.is_owner()
+@commands.has_any_role('Teacher')
 async def assignrole(ctx, user: discord.Member, *, role_name: str):
+    # Make sure person using command is the server owner or has role 'Teacher'
+    if not ctx.author == ctx.guild.owner and not discord.utils.get(ctx.author.roles, name="Teacher"):
+        await ctx.send("You don't have permission to use this command.")
+        return
+
     # Convert to lower case for case insensitivity.
     role_name_lower = role_name.lower()
 
@@ -68,5 +73,45 @@ async def assignrole(ctx, user: discord.Member, *, role_name: str):
     await user.add_roles(role)
     await ctx.send(f"{role.name} has been assigned to {user.name}.")
 
+
+@bot.command()
+@commands.has_any_role('Teacher')
+async def unassignrole(ctx, user: discord.Member, *, role_name: str):
+    # Make sure person using command is the server owner or has role 'Teacher'
+    if not ctx.author == ctx.guild.owner and not discord.utils.get(ctx.author.roles, name="Teacher"):
+        await ctx.send("You don't have permission to use this command.")
+        return
+
+    # Convert to lower case for case insensitivity.
+    role_name_lower = role_name.lower()
+
+    # Search for the role using the lowercase role name
+    role = discord.utils.find(lambda r: r.name.lower() == role_name_lower, ctx.guild.roles)
+
+    # if role does not exist in the server
+    if not role:
+        await ctx.send("Role not found!")
+        return
+
+    # If user doesn't have the specified role
+    if role not in user.roles:
+        await ctx.send(f"{user.name} doesn't have the {role.name} role!")
+        return
+
+    # Tell user they don't have permissions to use this command
+    bot_member = ctx.guild.me
+    if role >= bot_member.top_role:
+        await ctx.send("You don't have the permissions to unassign this role.")
+        return
+
+    # Make sure role exists
+    allowed_roles = ["student", "ta", "teacher"]
+    if role_name_lower not in allowed_roles:
+        await ctx.send("This role cannot be unassigned.")
+        return
+
+    # Remove the role from the user in the server
+    await user.remove_roles(role)
+    await ctx.send(f"{role.name} has been unassigned from {user.name}.")
 
 bot.run(TOKEN)
